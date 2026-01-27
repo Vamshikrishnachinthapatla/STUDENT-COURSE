62f5e523dc8b650742bfb6ae66451e785a7eb7d4
vamshikrishna <krishnachvamshi@gmail.com>
2026-01-27 16:40:01 +0530

    3rd commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import register_view, logout_view, dashboard
+from .api_views import RegisterAPI
 
 urlpatterns = [
     path('register/', register_view, name='register'),
     path('logout/', logout_view, name='logout'),
-    path('dashboard/', dashboard, name='dashboard')
+    path('dashboard/', dashboard, name='dashboard'),
+
+    path('api/register/', RegisterAPI.as_view()),
 ]
